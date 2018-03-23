1b81b18891334a323635eaa8667a0dcc233083f5
lazzzis <shenlijin@outlook.com>
2018-03-22 22:51:16 -0400

    remove version tag on request url

--- src/api.js
+++ src/api.js
@@ -4,14 +4,4 @@ const instance = axios.create({
   baseURL: window.root
 })
 
-const lastModified = window.lastModified || 0
-
-export default {
-  get (url) {
-    return instance.get(url, {
-      params: {
-        v: lastModified
-      }
-    })
-  }
-}
+export default instance
